397f573d52f0a9e0bd53442a6c0fba89699d9583
Q <Q@Skylars-MBP.lan1>
2020-10-20 10:05:58 -0700

    deployed url

--- src/App.js
+++ src/App.js
@@ -5,25 +5,98 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+
+ // URL VARIABLE
+ const url = "http://localhost:4500"
+ // STATE TO HOLD DOGS
+ const [dogs, setDogs] = React.useState([])
+ //Empty dog for Form
+ const emptyDog = {
+  name: "",
+  age: 0,
+  img: ""
+}
+
+
+//SelectedDog to select a dog for update
+const [selectedDog, setSelectedDog] = React.useState(emptyDog)
+
+//selectDog which selects a dog
+const selectDog = (dog) => {
+  setSelectedDog(dog)
+}
+
+ // FUNCTION TO FETCH DOGS
+ const getDogs = () => {
+   fetch(url + "/dog/")
+   .then(response => response.json())
+   .then(data => {
+     setDogs(data)
+   })
+ }
+ //Get dogs on page load
+ React.useEffect(() => {
+   getDogs()
+ }, [])
+ //handleCreate Function for creating dogs
+ const handleCreate = (newDog) => {
+   fetch(url+"/dog/", {
+     method: "post",
+     headers: {
+       "Content-Type": "application/json"
+     },
+     body: JSON.stringify(newDog)
+   })
+   .then(response => getDogs())
+ }
+ //handleUpdate to update a dog when form is clicked
+ const handleUpdate = (dog) => {
+  fetch(url + "/dog/" + dog._id, {
+    method: "put",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(dog)
+  })
+  .then(response => getDogs())
+}
+//deleteDog funtion to delete
+const deleteDog = (dog) => {
+  fetch(url + "/dog/" + dog.id, {
+    method: "delete"
+  })
+  .then(response => getDogs())
+}
+
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
-      <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+        
+          <Link to="/create">
+            <button>Add Dog</button>
+          </Link>
+          <main>
+          <Switch>
+          <Route exact path="/" render={(rp) =>( <Display {...rp} 
+          dogs={dogs} selectDog={selectDog}
+          deleteDog={deleteDog}/>
+          )}
+          />1
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="create" dog={emptyDog} handleSubmit = {handleCreate} />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="update" dog={selectedDog}
+               handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
